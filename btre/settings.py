4471c162ad87273b74df0a11435a1acf332f2ffd
AliML1992 <49343639+AliML1992@users.noreply.github.com>
2019-10-19 13:07:25 +0900

    Update settings.py

--- btre/settings.py
+++ btre/settings.py
@@ -141,9 +141,4 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
 
-# email config
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
-EMAIL_USE_TLS = True
+
